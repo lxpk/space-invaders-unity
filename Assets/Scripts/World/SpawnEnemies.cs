ac157b47fed67f317b07b092e361520a24b13eb7
hs.jason <hs.jason@c78648d1-4ad2-e23d-0f76-7fa68d1168a4>
2012-04-17 06:16:01 +0000

    Enabled Respawning Enemies

--- Assets/Scripts/World/SpawnEnemies.cs
+++ Assets/Scripts/World/SpawnEnemies.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class SpawnEnemies : MonoBehaviour {
 	
@@ -16,9 +16,23 @@ public class SpawnEnemies : MonoBehaviour {
 	public float RowSpacing;
 	public float ColumnSpacing;
 	
+	public float CutoffDistance;
+	
+	float timer;
+	
+
+	List<Transform> enemies;
 	
 	// Use this for initialization
 	void Start () {
+		timer = Time.time;
+		Spawn();
+	}
+	
+	void Spawn () {
+		
+		enemies = new List<Transform>();
+		
 		for (int z = 0; z < Enemy1Rows; z++)
 			FillRow(RowSpacing * z + FormationBottom, Enemy1);
 		for (int z = Enemy1Rows; z < Enemy1Rows+Enemy2Rows; z++)
@@ -33,12 +47,39 @@ public class SpawnEnemies : MonoBehaviour {
 		{
 			float xPos = offset + x * ColumnSpacing;
 			Vector3 pos = new Vector3(xPos, 0.0f, zPos);
-			Instantiate(EnemyType, pos, Quaternion.identity);
+			enemies.Add(Instantiate(EnemyType, pos, Quaternion.identity) as Transform);
 		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void CheckRespawn () {
+		bool stillLive = false;
+		foreach (var enemy in enemies) {
+			if (enemy != null) {
+				if (enemy.position.z > CutoffDistance){
+					stillLive = true;
+					break;
+				}
+				else
+				{
+					Destroy(enemy.gameObject);
+				}
+			}
+		}
+		
+		if (!stillLive)
+		{			
+			Spawn();
+		}
+		
+		
+	}
 	
+	// Update is called once per frame
+	
+	void Update () {
+		if (Time.time - timer > 0.5) {
+			CheckRespawn();
+			timer = Time.time;
+		}
 	}
 }
