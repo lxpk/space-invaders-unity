57d559c1187c80038a8e2e60ec051911658a0ff8
hs.jason <hs.jason@c78648d1-4ad2-e23d-0f76-7fa68d1168a4>
2012-04-20 05:36:00 +0000

    Implementing menu graphics

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,6 +3,20 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+
+    public Texture TitleTexture;
+    public int TitleWidth;
+    public int TitleHeight;
+    public int TitleTop;
+
+    public Texture StartGameTex;
+    public Texture SettingsTex;
+    public Texture QuitTex;
+
+    public int ButtonWidth;
+    public int ButtonHeight;
+    public int ButtonTop;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -18,14 +32,49 @@ public class MainMenu : MonoBehaviour {
 	}
 	
 	void OnGUI (){
-		
+
+        //Title
+        GUI.DrawTexture(
+            new Rect(
+                (Screen.width - TitleWidth) / 2, 
+                TitleTop, 
+                TitleWidth, 
+                TitleHeight), 
+            TitleTexture);
+
 		GUI.Label(WindowRect (0.4f, 0.1f, 0.2f, 0.1f), "High Score: " + PlayerPrefs.GetInt("HScore"));
-		
-		if (GUI.Button(WindowRect (0.3f, 0.3f, 0.4f, 0.2f), "Start Game")){
+
+        if (GUI.Button(
+            new Rect(
+                (Screen.width - ButtonWidth) / 2, 
+                ButtonTop, 
+                ButtonWidth, 
+                ButtonHeight), 
+            StartGameTex))
+        {
 			Application.LoadLevel("default");
 		}
-		
-		if (GUI.Button(WindowRect (0.3f, 0.6f, 0.4f, 0.2f), "Quit Game")){
+
+        if (GUI.Button(
+            new Rect(
+                (Screen.width - ButtonWidth) / 2,
+                ButtonTop + ButtonHeight, 
+                ButtonWidth, 
+                ButtonHeight), 
+            SettingsTex))
+        {
+            Application.LoadLevel("default");
+        }
+
+
+        if (GUI.Button(
+            new Rect(
+                (Screen.width - ButtonWidth) / 2, 
+                ButtonTop + 2 * ButtonHeight,
+                ButtonWidth, 
+                ButtonHeight), 
+            QuitTex))
+        {
 			Application.Quit();
 		}
 	}
