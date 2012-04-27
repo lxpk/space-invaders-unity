d14e2c93d114dff400a43a9e1bdeb7c7ba811480
hs.jason <hs.jason@c78648d1-4ad2-e23d-0f76-7fa68d1168a4>
2012-04-27 03:38:43 +0000

    -Adding autofire when n "swipe" mode

--- Assets/Scripts/Input/ShootControls.cs
+++ Assets/Scripts/Input/ShootControls.cs
@@ -9,12 +9,20 @@ public class ShootControls : MonoBehaviour {
 	float nextFireTime = 0.0f;
 	
 	public AudioClip FireSound;
+    bool isAutoFire = false;
 	
 	// Use this for initialization
 	void Start () {
-	    
+        string inputMode = "";
+        if (PlayerPrefs.HasKey("InputMode"))
+            inputMode = PlayerPrefs.GetString("InputMode");
+        if (inputMode == "Swipe")
+            isAutoFire = true;
+        else
+            isAutoFire = false;
 	}
 	
+
 	// Update is called once per frame
 	void Update () {
 	
